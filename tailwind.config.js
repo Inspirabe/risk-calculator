a4d3f41697fc6ccba520c310f0b19cc17d85b069
Robby Borgmans <robby@inspira.be>
2025-08-25 16:49:58 +0200

    tussentijdse commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{html,ts}",
+        "./node_modules/@ionic/angular/**/*.mjs"
+    ],
+    theme: {
+        extend: {
+            // Optioneel: koppel Tailwind-kleuren aan Ionic CSS vars
+            colors: {
+                primary: "var(--ion-color-primary)",
+                "primary-contrast": "var(--ion-color-primary-contrast)",
+                secondary: "var(--ion-color-secondary)",
+                muted: "var(--ion-color-medium)"
+            },
+        },
+    },
+    plugins: [],
 }
